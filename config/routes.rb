a0f83fa9cc200bddfd00c1c1892df42a83f2f3d0
Brian Bornino <brianbornino@gmail.com>
2023-09-25 14:55:23 -0700

    test commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  #get 'job_sites/index'
-  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
