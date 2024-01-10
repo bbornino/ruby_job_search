8033f6420245ce29216fcbff01af3caea241ff85
Brian Bornino <brianbornino@gmail.com>
2024-01-10 15:42:35 -0800

    No Longer Using CKEditor.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
+// import { ClassicEditor } from './ckeditor/ckeditor5-editor-classic';
+
 import "controllers"
 
 console.log('app/js/application.js Complete')
