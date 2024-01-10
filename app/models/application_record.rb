18e17110e68d7927a4672b64a0b8bfe551300c9b
Brian Bornino <brianbornino@gmail.com>
2024-01-10 15:50:18 -0800

    Application Record SQL Better Readability

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
   def self.applied_per_site
-    return ApplicationRecord.connection.select_all("SELECT s.name, COUNT(*) AS site_count 
+    return ApplicationRecord.connection.select_all("SELECT 
+                s.name, COUNT(*) AS site_count 
             FROM job_sites s JOIN job_postings p ON p.job_site_id = s.id 
             GROUP BY p.job_site_id")
   end
